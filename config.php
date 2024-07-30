b7bf8e7a259a93773587a7c91e2bcbb46727a3ec
Abdul Rehman <abdulrehman226721skp@gmail.com>
2024-07-31 02:14:14 +0500

    new code 2 db pass

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php 
 	session_start();
 	// connect to database
-	$conn = mysqli_connect("localhost", "u927419660_blog", "", "u927419660_blog");
+	$conn = mysqli_connect("localhost", "u927419660_blog", "0qEkxGoS7@", "u927419660_blog");
 
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
