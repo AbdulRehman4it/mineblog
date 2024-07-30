3b64db816dfaf2a295bc225a430867e64905deff
Abdul Rehman <abdulrehman226721skp@gmail.com>
2024-07-31 01:45:10 +0500

    new code

--- admin/inc/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="text-center">
-        Copyright &copy; by
-        <a href="#" class="text-white">Blogging Website</a> from 2021 - 2022
-      </footer>
--- index.php
+++ index.php
@@ -1,265 +1,275 @@
-<?php
-      require_once('./db.php'); 
-     require_once('./inc/head.php');
-     ?>
-  <body>
+<?php require("libs/fetch_data.php");?>
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title><?php getwebname("titles"); echo"|"; gettagline("titles");?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<link id="browser_favicon" rel="shortcut icon" href="blogadmin/images/<?php geticon("titles"); ?>">
+	<meta charset="utf-8" name="description" content="<?php getshortdescription("titles");?>">
+	<meta name="keywords" content="<?php getkeywords("titles");?>" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" href="css/jquery.desoslide.css">
+	<link href="css/style.css" rel='stylesheet' type='text/css' />
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800"
+	rel="stylesheet">
+</head>
+
+<body>
+	<?php include("header.php");?>
+	<?php include("banner.php");?>
+	<section class="bottom-slider">
+		<div class="course_demo1">
+			<ul id="flexiselDemo1">
+				<?php getbottomsliderposts("blogs");?>
+			</ul>
+		</div>
+		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-hidden="true">
+			<div class="modal-dialog" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							<span aria-hidden="true">&times;</span>
+						</button>
+					</div>
+					<div class="modal-body">
+						<div class="embed-responsive embed-responsive-21by9">
+							<iframe src="https://www.youtube.com"></iframe>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--/main-->
+	<section class="main-content-w3layouts-agileits">
+		<div class="container">
+			<div class="row">
+				<!--left-->
+				<div class="col-lg-8 left-blog-info-w3layouts-agileits text-left">
+					<!--grid blogs below-->
+					<div class="blog-girds-sec">
+						<div class="row">
+							<?php getblogridposts("blogs");?>
+							<!--bloggrids-->
+						</div>
+					</div>
+				</div>
+				<!--//left-->
+				<!--right-->
+				<aside class="col-lg-4 agileits-w3ls-right-blog-con text-right">
+					<div class="right-blog-info text-left">
+						<h4><strong>Categories</strong></h4>
+						<ul class="list-group single">
+							<?php countcategories();?>
+						</ul>
+						<div class="tech-btm widget_social">
+							<h4>Stay Connected</h4>
+							<ul>
+
+								<li>
+									<a class="twitter" href="<?php getlinks("links","twitter");?>">
+										<i class="fab fa-twitter"></i>
+										<span class="count"></span> Twitter</a>
+									</li>
+									<li>
+										<a class="facebook" href="<?php getlinks("links","facebook");?>">
+											<i class="fab fa-facebook-f"></i>
+											<span class="count"></span> Facebook</a>
+										</li>
+										<li>
+											<a class="dribble" href="<?php getlinks("links","dribble");?>">
+												<i class="fab fa-dribbble"></i>
+
+												<span class="count"></span> Dribble</a>
+											</li>
+											<li>
+												<a class="pin" href="<?php getlinks("links","pinterest");?>">
+													<i class="fab fa-pinterest"></i>
+													<span class="count"></span> Pinterest</a>
+												</li>
+
+											</ul>
+										</div>
+										<div class="tech-btm">
+											<h4>Older Posts</h4>
+											<?php getolderposts("blogs");?>
+											<!--olderpostsendhere-->
+										</div>
+									</div>
+								</aside>
+								<!--//right-->
+							</div>
+						</div>
+					</section>
+					<!--//main-->
+					<!--/middle-->
+					<section class="middle-sec-agileinfo-w3ls">
+						<div class="container">
+							<div class="row inner-sec">
+								<div class="col-md-4 news-left">
+									<ul id="demo1_thumbs" class="list-inline">
+										<!--get latest four-->
+										<?php getfour("blogs");?>
+									</ul>
+								</div>
+								<div id="demo1_main_image" class="col-md-8  news-right"></div>
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+
+					</section>
+					<!--//middle-->
+					<!---->
+					<section class="main-content-w3layouts-agileits">
+						<div class="container">
+							<div class="row">
+								<!--left-->
+								<div class="col-lg-8 left-blog-info-w3layouts-agileits text-left">
+									<?php getonelatest("blogs");?>
+								</div>
+								<!--//left-->
+								<!--right-->
+								<aside class="col-lg-4 agileits-w3ls-right-blog-con text-left">
+									<div class="tech-btm">
+										<h4>Editor's Choice</h4>
+										
+										<?php geteditorschoice("editors_choice"); ?>
+										
+									</div>
+									
+								</aside>
+								<!--//right-->
+							</div>
+						</div>
+					</section>
+					<!--//main-->
+					<?php include("footer.php");?>
+					<!---->
+					<!-- js -->
+					<script src="js/jquery-2.2.3.min.js"></script>
+					<!-- //js -->
+					<!-- desoslide-JavaScript -->
+					<script src="js/jquery.desoslide.js"></script>
+					<script>
+						$('#demo1_thumbs').desoSlide({
+							main: {
+								container: '#demo1_main_image',
+								cssClass: 'img-responsive'
+							},
+							effect: 'sideFade',
+							caption: true
+						});
+					</script>
+
+					<!-- requried-jsfiles-for owl -->
+					<script>
+						$(window).load(function () {
+							$("#flexiselDemo1").flexisel({
+								visibleItems: 3,
+								animationSpeed: 1000,
+								autoPlay: true,
+								autoPlaySpeed: 3000,
+								pauseOnHover: true,
+								enableResponsiveBreakpoints: true,
+								responsiveBreakpoints: {
+									portrait: {
+										changePoint: 480,
+										visibleItems: 1
+									},
+									landscape: {
+										changePoint: 640,
+										visibleItems: 2
+									},
+									tablet: {
+										changePoint: 768,
+										visibleItems: 3
+									}
+								}
+							});
+
+						});
+					</script>
+					<script>
+						$(window).load(function () {
+							$("#flexiselDemo2").flexisel({
+								visibleItems: 3,
+								animationSpeed: 1000,
+								autoPlay: true,
+								autoPlaySpeed: 3000,
+								pauseOnHover: true,
+								enableResponsiveBreakpoints: true,
+								responsiveBreakpoints: {
+									portrait: {
+										changePoint: 480,
+										visibleItems: 1
+									},
+									landscape: {
+										changePoint: 640,
+										visibleItems: 2
+									},
+									tablet: {
+										changePoint: 768,
+										visibleItems: 3
+									}
+								}
+							});
+
+						});
+					</script>
+					<script src="js/jquery.flexisel.js"></script>
+					<!-- //password-script -->
+					<!--/ start-smoth-scrolling -->
+					<script src="js/move-top.js"></script>
+					<script src="js/easing.js"></script>
+					<script>
+						jQuery(document).ready(function ($) {
+							$(".scroll").click(function (event) {
+								event.preventDefault();
+								$('html,body').animate({
+									scrollTop: $(this.hash).offset().top
+								}, 900);
+							});
+						});
+					</script>
+					<!--// end-smoth-scrolling -->
+
+					<script>
+						$(document).ready(function () {
+							
+							var defaults = {
+							  			containerID: 'toTop', // fading element id
+										containerHoverID: 'toTopHover', // fading element hover id
+										scrollSpeed: 1200,
+										easingType: 'linear' 
+									};
+									
+
+									$().UItoTop({
+										easingType: 'easeOutQuart'
+									});
+
+								});
+							</script>
+							<a href="#home" class="scroll" id="toTop" style="display: block;">
+								<span id="toTopHover" style="opacity: 1;"> </span>
+							</a>
+
+							<!-- //Custom-JavaScript-File-Links -->
+							<script src="js/bootstrap.js"></script>
 
 
-  <?php
-     require_once('./inc/nav.php');
-     ?>
+						</body>
 
-
-
-<?php
-    if (isset($_GET['cat'])) {
-        $cat_id = $_GET['cat'];
-        $cat_query = "SELECT * FROM categories WHERE id = $cat_id";
-        $cat_run = mysqli_query($conn, $cat_query);
-        $cat_row = mysqli_fetch_array($cat_run);
-        $cat_name = $cat_row['category']; 
-      }
-    ?>
-   
-
-
- <!-- hero section start -->
- <?php
-   $query = "SELECT * FROM blogs";
-   if (isset($cat_name)) {
-       $query .= " WHERE categories = '$cat_name'";
-   }
-   else{
-   $query .= " ORDER BY rand() LIMIT 2";
-  }
-   $run = mysqli_query($conn, $query);
-
-   if ($run && mysqli_num_rows($run) > 0) {
-       $row = mysqli_fetch_array($run);
-       // Display the first blog
-       ?>
-
-<div class="">
-<section
-      class="bg-[url('./assets/img/her-bg.png')] h-[70vh] bg-center lg:bg-top bg-cover bg-no-repeat flex items-end"
-    >
-      <div class="pl-[5%] pb-8">
-        <div
-          class="text-base nimbusl-bold text-[#AEAEAE] flex items-center gap-10"
-        >
-
-        <?php 
-                                $id = $row['id'];
-                                $timestamp = strtotime($row['date']); 
-                                $date = getdate($timestamp); 
-                                // $day = $date['mday'];
-                                $month = $date['month'];
-                                $year = $date['year'];
-                            ?>
-          <p> <?php echo $month;?>, <?php echo $year;?></p>
-          <ul class="list-disc">
-            <li><?php echo ucfirst($row['categories']);?></li>
-          </ul>
-        </div>
-        <a href="./blog.php?cat_id=<?php echo $row['categories_id'];?>">
-        <h1 class="text-white text-3xl lg:text-4xl nimbusl-bold">
-        <?php echo $row['first_title'];?> <span class="ogg"><?php echo $row['second_title'];?>  <i><?php echo $row['third_title'];?></i></span>
-        </h1>
-        </a>
-
-      </div>
-    </section>
-
-</div>
-   
-    <?php
-    } else {
-        echo "<h2 class='text-back'>No Blogs Available</h2>";
-    }
-    
-
-// Fetch the video
-    $query = "SELECT * FROM video_blogs LIMIT 1";
-    $run = mysqli_query($conn, $query);
-
-    if ($run && mysqli_num_rows($run) > 0) {
-        $row = mysqli_fetch_array($run);
-        // Display the video
-        ?>
-    <section class="lg:h-[70vh] relative overflow-y-hidden top-8 lg:top-20">
-      <div>
-        <video width="100%" height="100%"  autoplay loop>
-          <source
-            src="./assets/img/<?php echo $row['video']; ?>"
-            type="video/mp4"
-          />
-          <!-- <source src="movie.ogg" type="video/ogg"> -->
-          Your browser does not support the video tag.
-        </video>
-      </div>
-      <div class="pl-[5%] lg:pb-8 absolute bottom-4 lg:bottom-10">
-      <?php 
-                    $timestamp = strtotime($row['date']); 
-                    $date = getdate($timestamp); 
-                    // $day = $date['mday'];
-                    $month = $date['month'];
-                    $year = $date['year'];
-                ?>
-        <div
-          class="text-base nimbusl-bold text-[#AEAEAE] flex items-center gap-10"
-        >
-          <p> <?php echo $month;?>, <?php echo $year;?></p>
-          <ul class="list-disc">
-            <li><?php echo ucfirst($row['categories']);?></li>
-          </ul>
-        </div>
-        <h1 class="text-white text-3xl lg:text-4xl nimbusl-bold">
-        <?php echo $row['first_title'];?><span class="ogg"><?php echo $row['second_title'];?></span>
-        </h1>
-      </div>
-    </section>
-
-    <?php
-    } else {
-        echo "<h2 class='text-back'>No Video Available</h2>";
-    }
-
-    ?>
-
-    <section class="p-[5%] pt-16 lg:pt-[10%]">
-      <img src="./assets/img/google-add.png" class="w-full" alt="" />
-    </section>
-
-
-    <!--slider  -->
-    <?php
-    $query = "SELECT * FROM blogs";
-    if (isset($cat_name)) {
-        $query .= " WHERE categories = '$cat_name'";
-    }
-    $result = mysqli_query($conn, $query);
-    if ($result) {
-        if (mysqli_num_rows($result) > 0) {
-    ?>
-
-
-
-    <section class="bg-black p-[5%]">
-      <h2 class="text-3xl nimbusl-bold text-white uppercase pb-5">
-        Related Blogs
-      </h2>
-      <!-- Swiper -->
-      <div class="swiper mySwiper">
-        <div class="swiper-wrapper pb-16">
-        <?php
-                       while ($row = mysqli_fetch_assoc($result)) {
-                      ?>  
-          <div class="swiper-slide bg-white p-3 lg:p-8">
-          <a href="blog_page.php?blog_id=<?php echo $row['id']; ?>">
-            <div
-              class="bg-[url('./assets/img/<?php echo $row['image']; ?>')] h-[500px] bg-cover bg-no-repeat flex items-end"
-            >
-              <div class="p-5">
-                
-
-
-                <a href="./blog.php?cat_id=<?php echo $row['categories_id'];?>">
-        <h1 class="text-white text-xl  ogg">
-        <?php echo $row['first_title'];?> <span class="ogg"><?php echo $row['second_title'];?>  <i><?php echo $row['third_title'];?></i></span>
-        </h1>
-        </a>
-
-
-
-
-
-                
-              </div>
-            </div>
-                       </a>
-          </div>
-
-          <?php
-                            }
-                            ?>
-                    
-
-         
-        </div>
-        <div class="swiper-pagination"></div>
-      </div>
-    </section>
-    <?php
-        } else {
-           
-            echo "<p>No related blogs found.</p>";
-        }
-    } else {
-     
-        echo "Error: " . mysqli_error($conn);
-    }
-    ?>
-     <!-- slider -->
-     <?php
-    // Fetch the remaining blogs
-    $query = "SELECT * FROM blogs";
-    if (isset($cat_name)) {
-        $query .= " WHERE categories='$cat_name'";
-    }
-    else{
-      $query .= " ORDER BY rand() LIMIT 3";
-    }
-    $run = mysqli_query($conn, $query);
-
-    if ($run && mysqli_num_rows($run) > 1) { // Check if there are more than one blogs
-        // Skip the first blog
-        mysqli_data_seek($run, 1);
-        
-        // Display the remaining blogs
-        while ($row = mysqli_fetch_array($run)) {
-            // Your code to display the remaining blogs goes here
-            ?>
-
-
-    <section
-      class="bg-[url('./assets/img/<?php echo $row['image']; ?>')] h-[70vh] bg-center bg-cover bg-no-repeat flex items-end mt-20"
-    >
-      <div class="pl-[5%] pb-8">
-        <div
-          class="text-base nimbusl-bold text-[#AEAEAE] flex items-center gap-10"
-        >
-        <?php 
-                            $timestamp = strtotime($row['date']); 
-                            $date = getdate($timestamp); 
-                            // $day = $date['mday'];
-                            $month = $date['month'];
-                            $year = $date['year'];
-                        ?>
-          <p> <?php echo $month;?>, <?php echo $year;?></p>
-          <ul class="list-disc">
-            <li><?php echo ucfirst($row['categories']);?></li>
-          </ul>
-        </div>
-        <h1 class="text-white text-3xl lg:text-4xl nimbusl-bold">
-        <?php echo $row['first_title'];?> <?php echo $row['second_title'];?> <span class="ogg"><i><?php echo $row['third_title'];?></i> </span>
-        </h1>
-      </div>
-    </section>
-    <?php
-        }
-    } else {
-        echo "<h2 class='text-back'>No More Blogs Available</h2>";
-    }
-
-    ?>
-   
-
-    <section class="py-[5%] px-[15%]">
-      <img src="./assets/img/google-add.png" class="w-full" alt="" />
-    </section>
-
-
-
-    <?php
-     require_once('./inc/footer.php');
-     ?>
+						</html>
