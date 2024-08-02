4fe52537c40ebd18bb60757cf5b3197e96518906
Abdul Rehman <abdulrehman226721skp@gmail.com>
2024-08-02 10:29:01 +0500

    google ads unit 2

--- index.php
+++ index.php
@@ -233,7 +233,18 @@
     </section>
 
 
-
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1761214904388955"
+     crossorigin="anonymous"></script>
+<!-- bloging rectangle -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-1761214904388955"
+     data-ad-slot="5751460752"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
     <?php
      require_once('./inc/footer.php');
      ?>
