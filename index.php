cf2e984d0f44c093f7731c0a65df65aefc1ffd30
Abdul Rehman <abdulrehman226721skp@gmail.com>
2024-08-02 11:03:37 +0500

    google ads unit index

--- index.php
+++ index.php
@@ -236,11 +236,11 @@
     
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1761214904388955"
      crossorigin="anonymous"></script>
-<!-- bloging rectangle -->
+<!-- rectangle 2 -->
 <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-1761214904388955"
-     data-ad-slot="5751460752"
+     data-ad-slot="2087715781"
      data-ad-format="auto"
      data-full-width-responsive="true"></ins>
 <script>
